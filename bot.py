e0ff1e0d23b513c7e6528e1270c2fd6ae3f095f7
Sachin79 <amanraw2@gmail.com>
2022-08-15 20:48:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ with bot:
 
 auth_users = [ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
 sudo_groups = [ int(chat) for chat in os.environ.get("GROUPS").split(",")  if chat != '']
-sudo_json_groups = [ int(chat) for chat in os.environ.get("HTML_GROUPS").split(",")  if chat != '']
+sudo_html_groups = [ int(chat) for chat in os.environ.get("HTML_GROUPS").split(",")  if chat != '']
 sudo_users = auth_users
 thumb = os.environ.get("THUMB")
 if thumb.startswith("http://") or thumb.startswith("https://"):
